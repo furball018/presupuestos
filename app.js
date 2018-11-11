5c9ea7dd535ab6f397e3f60ae6284c41843e1cf9
furball018 <margonbra@gmail.com>
2018-11-11 19:52:53 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        budgets: [
+            {
+                'nombre': 'presupuesto 1',
+                'precio': 30
+            },
+            {
+                'nombre': 'presupuesto 2',
+                'precio': 40
+            },
+            {
+                'nombre': 'presupuesto 3',
+                'precio': 50
+            },
+        ]
+    }
+});
