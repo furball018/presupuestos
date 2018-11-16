0ae3d3b5f203cb6fd03036680b1e7fd259bbe078
furball018 <margonbra@gmail.com>
2018-11-16 18:05:56 -0300

    Updated

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ var calcularotApp = new Vue({
         selected: '',
         precios: preciosData, 
         laserColor:{
-            title: 'Nuevo Presupuesto',
+            title: 'Trabajo Laser Color',
             papel: 'select',
             dimensiones: {
                 x: 0,
@@ -345,6 +345,14 @@ var budgetApp = new Vue({
             }else{
                 t *= Math.abs((this.discount.amount / 100) -1);
             }
+
+            return t.toFixed(2);
+        },
+        calcTotalNoDisc: function(){
+            var t = 0;
+            this.budgets.forEach(b => {
+                t += b.cost;
+            });
 
             return t.toFixed(2);
         }
